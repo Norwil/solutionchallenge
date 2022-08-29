61d7c69409a425c09ff1f352eb375895952548e2
Norwil <46763365+Norwil@users.noreply.github.com>
2022-08-29 23:56:22 +0200

    Update basicOperations.java

--- basicOperations.java
+++ basicOperations.java
@@ -1,5 +1,19 @@
-package challenge;
-
-public class basicOperations {
-
+public class BasicOperations{
+	
+  public static Integer basicMath(String op, int v1, int v2)
+  {
+    if(op == "+") {
+      return v1+v2;
+    }
+    else if(op == "-"){
+      return v1-v2;
+    }
+    else if(op == "*"){
+      return v1*v2;
+    }
+    else if(op == "/"){
+      return v1/v2;
+    }
+    return 0;
+  }
 }
